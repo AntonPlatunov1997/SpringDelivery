1c41d27aa873f7d5e399145dd0f98a6400ce64fe
antonplatov <xanton.platovx@gmail.com>
2019-06-18 20:14:46 +0300

    added CityRepository

--- src/main/java/com/example/deliveryPet/model/repository/CityRepository.java
+++ src/main/java/com/example/deliveryPet/model/repository/CityRepository.java
@@ -1,4 +1,8 @@
 package com.example.deliveryPet.model.repository;
 
-public class CityRepository {
+import com.example.deliveryPet.model.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City,Long> {
+    City findByCityName(String name);
 }
